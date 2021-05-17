65acdeb65620295415d08fd7ad5f440e2227916a
jgenero <jeandamien.genero@gmail.com>
2021-05-17 13:49:31 +0200

    changing path with new repository name

--- app/constantes.py
+++ app/constantes.py
@@ -16,16 +16,16 @@ from lxml import etree
 RESULT_PAR_PAGES = 5
 
 # Parsage du document XML-TEI avec la méthode .parse() d'etree, et stockage du résultat dans source_doc :
-source_doc = etree.parse("../Actes_Charles_Ier/app/static/xml/corpus-act-ch.xml")
+source_doc = etree.parse("../actes-bourbon/app/static/xml/corpus-act-ch.xml")
 
 # Parsage de la feuille de transformation XSL de la partie "Projet" avec la méthode .parse() d'etree, et stockage du résultat dans xslt_projet_doc :
-xslt_projet_doc = etree.parse("../Actes_Charles_Ier/app/static/xslt/projet-xslt.xslt")
+xslt_projet_doc = etree.parse("../actes-bourbon/app/static/xslt/projet-xslt.xslt")
 
 # Parsage de la feuille de transformation XSL de la partie "Index nominum" avec la méthode .parse() d'etree, et stockage du résultat dans xslt_index_nom_doc :
-xslt_index_nom_doc = etree.parse("../Actes_Charles_Ier/app/static/xslt/index-nominum.xsl")
+xslt_index_nom_doc = etree.parse("../actes-bourbon/app/static/xslt/index-nominum.xsl")
 
 # Parsage de la feuille de transformation XSL de la partie "Index prosopographique" avec la méthode .parse() d'etree, et stockage du résultat dans xslt_index_prosopo_doc :
-xslt_index_prosopo_doc = etree.parse("../Actes_Charles_Ier/app/static/xslt/index-xslt.xsl")
+xslt_index_prosopo_doc = etree.parse("../actes-bourbon/app/static/xslt/index-xslt.xsl")
 
 # Parsage de la feuille de transformation XSL de la partie "Index locorum" avec la méthode .parse() d'etree, et stockage du résultat dans xslt_index_lieux_doc :
-xslt_index_lieux_doc = etree.parse("../Actes_Charles_Ier/app/static/xslt/index-locorum.xsl")
+xslt_index_lieux_doc = etree.parse("../actes-bourbon/app/static/xslt/index-locorum.xsl")
