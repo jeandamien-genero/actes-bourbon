f97197b407851879a3be1446c7580d8281d7a5ef
jgenero <jeandamien.genero@gmail.com>
2021-06-19 15:42:59 +0200

    doctring + imports

---
+++ app/geocoder.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+"""
+    Définition des routes de l'application
+    author : Jean-Damien Généro
+    date : 2021-06-19
+    update : 
+"""
+
+import geopy
+import pandas
+from geopy.geocoders import Nominatim, GoogleV3
