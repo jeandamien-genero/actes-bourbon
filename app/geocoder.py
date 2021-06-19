be57e617540389800ba49dd591129627716f072d
jgenero <jeandamien.genero@gmail.com>
2021-06-19 15:58:06 +0200

    full function

--- app/geocoder.py
+++ app/geocoder.py
@@ -15,12 +15,23 @@ from geopy.geocoders import Nominatim, GoogleV3
 
 
 def main():
-	io = pandas.read_csv('static/csv/census-historic-population-borough.csv', index_col=None, header=0, sep=",")
+	io = pandas.read_csv('./static/csv/census-historic-population-borough.csv', index_col=None, header=0, sep=",")
 
 
-def get_latitude(x):
-  return x.latitude
+	def get_latitude(x):
+	  return x.latitude
+	
+	
+	def get_longitude(x):
+	  return x.longitude
 
 
-def get_longitude(x):
-  return x.longitude
+	geolocator = Nominatim(user_agent="You")
+	geolocate_column = io['Area_Name'].apply(geolocator.geocode)
+	io['latitude'] = geolocate_column.apply(get_latitude)
+	io['longitude'] = geolocate_column.apply(get_longitude)
+	io.to_csv('geocoding-output.csv')
+
+
+if __name__ == '__main__':
+  main()
