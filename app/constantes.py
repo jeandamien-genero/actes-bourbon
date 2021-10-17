0c5a68c4a1a6ee9b054402efcb84ca4648c6a74b
Jean-Damien Genero <jean-damien.genero@ehess.fr>
2021-10-17 17:53:17 +0200

    deleting NS in XML for XSL transformation

--- app/constantes.py
+++ app/constantes.py
@@ -17,6 +17,12 @@ RESULT_PAR_PAGES = 5
 
 # Parsage du document XML-TEI avec la méthode .parse() d'etree, et stockage du résultat dans source_doc :
 source_doc = etree.parse("../actes-bourbon/app/static/xml/corpus-act-ch.xml")
+# Suppression du nom de domaine pour permettre la transformation XSLT
+query = "descendant-or-self::*[namespace-uri()!='']"
+for element in source_doc.xpath(query):
+    #replace element name with its local name
+    element.tag = etree.QName(element).localname
+etree.cleanup_namespaces(source_doc)
 
 # Parsage de la feuille de transformation XSL de la partie "Projet" avec la méthode .parse() d'etree, et stockage du résultat dans xslt_projet_doc :
 xslt_projet_doc = etree.parse("../actes-bourbon/app/static/xslt/projet-xslt.xslt")
